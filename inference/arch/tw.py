b9b5ae14911319aefb70e785e9dfdc31f2f0f216
Bismarck Bamfo Odoom <bismarckbamfo91@gmail.com>
2022-01-02 05:57:17 +0000

    Model architecture for inference

---
+++ inference/arch/tw.py
@@ -0,0 +1,74 @@
+import torch.nn as nn
+from arch.transformation import TPS_SpatialTransformerNetwork
+from arch.feature_extraction import VGG_FeatureExtractor, RCNN_FeatureExtractor, ResNet_FeatureExtractor
+from arch.sequence_modeling import BidirectionalLSTM
+from arch.prediction import Attention
+
+class Model(nn.Module):
+
+    def __init__(self, input_channel, output_channel, hidden_size, num_class, opt):
+        super(Model, self).__init__()
+        self.opt = opt
+        self.stages = {'Trans': self.opt.Transformation, 'Feat': self.opt.FeatureExtraction,
+                       'Seq': self.opt.SequenceModeling, 'Pred': self.opt.Prediction}
+
+        """ Transformation """
+        if self.opt.Transformation == 'TPS':
+            self.Transformation = TPS_SpatialTransformerNetwork(
+                F=self.opt.num_fiducial, I_size=(self.opt.imgH, self.opt.imgW), I_r_size=(self.opt.imgH, self.opt.imgW), I_channel_num=input_channel)
+        else:
+            print('No Transformation module specified')
+
+        """ FeatureExtraction """
+        if self.opt.FeatureExtraction == 'VGG':
+            self.FeatureExtraction = VGG_FeatureExtractor(input_channel, output_channel)
+        elif self.opt.FeatureExtraction == 'RCNN':
+            self.FeatureExtraction = RCNN_FeatureExtractor(input_channel, output_channel)
+        elif self.opt.FeatureExtraction == 'ResNet':
+            self.FeatureExtraction = ResNet_FeatureExtractor(input_channel, output_channel)
+        else:
+            raise Exception('No FeatureExtraction module specified')
+        self.FeatureExtraction_output = output_channel  # int(imgH/16-1) * 512
+        self.AdaptiveAvgPool = nn.AdaptiveAvgPool2d((None, 1))  # Transform final (imgH/16-1) -> 1
+
+        """ Sequence modeling"""
+        if self.opt.SequenceModeling == 'BiLSTM':
+            self.SequenceModeling = nn.Sequential(
+                BidirectionalLSTM(self.FeatureExtraction_output, hidden_size, hidden_size),
+                BidirectionalLSTM(hidden_size, hidden_size, hidden_size))
+            self.SequenceModeling_output = hidden_size
+        else:
+            print('No SequenceModeling module specified')
+            self.SequenceModeling_output = self.FeatureExtraction_output
+
+        """ Prediction """
+        if self.opt.Prediction == 'CTC':
+            self.Prediction = nn.Linear(self.SequenceModeling_output, num_class)
+        elif self.opt.Prediction == 'Attn':
+            self.Prediction = Attention(self.SequenceModeling_output, hidden_size, num_class)
+        else:
+            raise Exception('Prediction is neither CTC or Attn')
+
+    def forward(self, input, text, is_train=False):
+        """ Transformation stage """
+        if not self.stages['Trans'] == "None":
+            input = self.Transformation(input)
+
+        """ Feature extraction stage """
+        visual_feature = self.FeatureExtraction(input)
+        visual_feature = self.AdaptiveAvgPool(visual_feature.permute(0, 3, 1, 2))  # [b, c, h, w] -> [b, w, c, h]
+        visual_feature = visual_feature.squeeze(3)
+
+        """ Sequence modeling stage """
+        if self.stages['Seq'] == 'BiLSTM':
+            contextual_feature = self.SequenceModeling(visual_feature)
+        else:
+            contextual_feature = visual_feature  # for convenience. this is NOT contextually modeled by BiLSTM
+
+        """ Prediction stage """
+        if self.stages['Pred'] == 'CTC':
+            prediction = self.Prediction(contextual_feature.contiguous())
+        else:
+            prediction = self.Prediction(contextual_feature.contiguous(), text, is_train, batch_max_length=self.opt.batch_max_length)
+
+        return prediction
