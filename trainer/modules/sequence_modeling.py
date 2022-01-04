4e75ecd282e2f5965b4929e38857b7473d17fd75
Bismarck Bamfo Odoom <bismarckbamfo91@gmail.com>
2022-01-04 17:33:39 +0000

    Implemented Bidirectional GRU

--- trainer/modules/sequence_modeling.py
+++ trainer/modules/sequence_modeling.py
@@ -20,3 +20,23 @@ class BidirectionalLSTM(nn.Module):
         recurrent, _ = self.rnn(input)  # batch_size x T x input_size -> batch_size x T x (2*hidden_size)
         output = self.linear(recurrent)  # batch_size x T x output_size
         return output
+
+class BidirectionalGRU(nn.Module):
+
+    def __init__(self, input_size, hidden_size, output_size):
+        super(GRU, self).__init__()
+        self.rnn = nn.GRU(input_size, hidden_size, biredirectional=True, batch_first=True)
+        self.linear = nn.Linear(hidden_size * 2, output_size)
+
+    def forward(self, input):
+        """
+        input : visual feature [batch_size X T X input_size]
+        output : contextual feature [batch_szie x output_size]
+        """
+        try:
+            self.rnn.flatten_parameters()
+        except:
+            pass
+        recurrent, _ = self.rnn(input)  # batch_size x T x input_size -> batch_size x T x (2*hidden_size)
+        output = self.linear(recurrent)  # batch_size x T x output_size
+        return output
