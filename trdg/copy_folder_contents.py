b79d151d6e0a4bd33410a68f72e30ce99d3e56e1
Bismarck Bamfo Odoom <bismarckbamfo91@gmail.com>
2022-01-13 10:16:03 +0000

    Copy folder contents

--- trdg/copy_folder_contents.py
+++ trdg/copy_folder_contents.py
@@ -1,14 +1,13 @@
 import os
 import shutil
 import sys
-from tqdm import trange
 
 def copy_files():
     src_directories = sys.argv[1:-1]
     tgt_directories = sys.argv[-1]
-    for directory in trange(src_directories):
+    for directory in src_directories:
         files = os.listdir(directory)
-        for f in trange(files):
+        for f in files:
             if f.endswith('.jpg') or f.endswith('.png'):
                 src = f'{directory.rstrip("/")}/{f}'
                 tgt = f'{tgt_directories.rstrip("/")}/{f}'
