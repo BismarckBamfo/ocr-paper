ec21146036b0d3f82e30fa465250ae500ffb03ac
Bismarck Bamfo Odoom <bismarckbamfo91@gmail.com>
2022-01-13 10:17:27 +0000

    Combine labels from all dataset parts into one file

---
+++ trdg/combine_labels.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+'''
+Combine labels to one lables.txt file and copy to all_data folder
+python combine_labels.py path1, path2, path3
+'''
+def combine_labels():
+    path1, path2, path3, path4 = sys.argv[1:]
+
+    with open(f'{path1}/labels.txt') as f:
+        text1 = f.readlines()
+
+
+    with open(f'{path2}/labels.txt') as f:
+        text2 = f.readlines()
+
+
+    with open(f'{path3}/labels.txt') as f:
+        text3 = f.readlines()
+
+    with open(f'{path4}/labels.txt') as f:
+        text4 = f.readlines()
+
+    text1.extend(text2)
+    text1.extend(text3)
+    text1.extend(text4)
+
+
+    with open(f'all_data/labels.txt', 'w', encoding='utf8') as f:
+        f.writelines(text1)
+
+
+def main():
+    combine_labels()
+
+
+if __name__ == '__main__':
+    main()
